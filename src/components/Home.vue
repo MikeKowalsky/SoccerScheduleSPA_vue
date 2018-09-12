249517d03d859145d170f832329efe9c590d15fd
Michal Kowalewski <mkowalewski83@gmail.com>
2018-09-12 12:30:26 +0200

    added  ajax calls

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,12 +6,13 @@
     <p>{{currUser}}</p>
 
     <button @click="logout">Logout</button>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -20,7 +21,27 @@ export default {
   data() {
     return {
       currUser: firebase.auth().currentUser.email,
+      testClubs: this.$store.state.clubsInfo,
+      testGames: this.$store.state.gamesInfo,
     };
+  },
+  mounted() {
+    axios.get('https://api.myjson.com/bins/16vy4g') // get clubs info
+      .then((response) => {
+        this.$store.commit('setClubs', response.data.clubs); // set the clubsInfo in the store
+      })
+    // eslint-disable-next-line
+    .catch(error => alert(error)); // handle error
+
+    axios.get('https://api.myjson.com/bins/nu2rk') // get games info
+      .then((response) => {
+        this.$store.commit('setGames', response.data.rounds); // set the clubsInfo in the store
+      })
+    // eslint-disable-next-line
+    .catch(error => alert(error)); // handle error
+
+    console.log(this.$store.state.clubsInfo);
+    console.log(this.$store.state.gamesInfo);
   },
   methods: {
     logout() {
@@ -38,11 +59,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .home{
+    color: white;
+  }
   button{
-      border: 1px solid black;
-      border-radius: 5px;
-      margin: 3px;
-      padding: 3px;
+    border: 1px solid black;
+    border-radius: 5px;
+    margin: 3px;
+    padding: 3px;
   }
 </style>
 
--- src/store.js
+++ src/store.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    clubsInfo: null,
+    gamesInfo: null,
   },
   mutations: {
-
+    setClubs(state, clubs) {
+      // eslint-disable-next-line
+      state.clubsInfo = clubs;
+    },
+    setGames(state, games) {
+      // eslint-disable-next-line
+      state.gamesInfo = games;
+    },
   },
   actions: {
 
