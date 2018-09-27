aa83a8c7959c29d167a695f41f10606f23622025
Michal Kowalewski <mkowalewski83@gmail.com>
2018-09-27 16:04:47 +0200

    working with stadiums and chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -5,27 +5,36 @@
     <div class="chat-content d-flex flex-column align-items-center">
       <p class="my-4 h2 text-danger font-weight-bold">Chat</p>
 
-      <div v-if="isLoading" class="posts-container d-flex flex-column align-items-start">
+      <div v-if="!isLoading"
+        class="posts-container d-flex flex-column align-items-start"
+        :class="{postsWithKeyboard: keyboard}" id="here">
         <div
           v-for="(item, key) in messages" :key="key"
           class="post" :class="{myPosts: item.name == user}">
           <span>{{item.name}}</span>
-          <p class="">{{item.body}}</p>
+          <p class="msgBody">{{item.body}}</p>
           <span>{{item.date}}</span>
         </div>
+
+        <div class="input d-flex align-items-center">
+          <!-- <b-form-input v-model="messageInput"
+            class="my-1 mr-2"
+            type="text"
+            @focus.native="keyboard = true"
+            @blur.native="keyboard = false"
+            placeholder="Enter your message"></b-form-input> -->
+          <b-form-input v-model="messageInput"
+            class="my-1 mr-2"
+            type="text"
+            placeholder="Enter your message"></b-form-input>
+          <b-button
+            class="my-2" variant="danger" @click="writeNewPost">
+            Send
+          </b-button>
+        </div>
+
       </div>
 
-      <div class="input">
-        <b-form-input v-model="messageInput"
-          class="my-1"
-          type="text"
-          placeholder="Enter your message"></b-form-input>
-      </div>
-
-      <b-button
-        class="my-2" size="lg" variant="danger" @click="writeNewPost">
-        Send
-      </b-button>
     </div>
 
     <Arrows />
@@ -55,12 +64,19 @@ export default {
       user: null,
       messages: null,
       isLoading: true,
+      keyboard: false,
     };
+  },
+  updated() {
+    const div = document.getElementById('here');
+    console.log(div);
+    console.log(div.scrollHeight);
+    div.scrollTop = div.scrollHeight - div.clientHeight;
   },
   mounted() {
     this.user = this.$store.state.user;
     this.getPosts();
-    this.isLoadingInt = false;
+    this.isLoading = false;
   },
   methods: {
     writeNewPost() {
@@ -78,11 +94,17 @@ export default {
       firebase.database().ref('mainChat').update(updates);
       this.messageInput = null;
       this.getPosts();
+      this.scrollThisShit();
     },
     getPosts() {
       firebase.database().ref('mainChat').on('value', (data) => {
         this.messages = data.val();
       });
+    },
+    scrollThisShit() {
+      const div = document.getElementById('here');
+      console.log(div);
+      div.scrollTop = div.scrollHeight - div.clientHeight;
     },
   },
 };
@@ -90,16 +112,27 @@ export default {
 
 <style lang="scss" scoped>
   .chat-content{
-    padding: 60px 0;
+    // padding: 60px 0 120px 0;
     height: auto;
+    // height: calc(100vh - 120px);
+    // padding-top: 60px;
+    padding: 60px 0;
     width: 100vw;
     overflow-y: scroll;
   }
   .posts-container{
     width: 90vw;
   }
+  .postsWithKeyboard{
+    height: 100px;
+  }
   .input{
-    width: 80%;
+    // position: fixed;
+    // top: calc(100vh - 120px);
+    // // top: 450px;
+    // margin-left: 10px;
+    height: 60px;
+    width: 90%;
   }
   .post{
     border: 1px solid red;
@@ -119,5 +152,9 @@ export default {
   .myPosts{
     text-align: right;
     align-self: flex-end;
+  }
+  .msgBody{
+    max-width: 85vw;
+    word-break: break-all;
   }
 </style>
--- src/components/Stadiums.vue
+++ src/components/Stadiums.vue
@@ -7,19 +7,47 @@
     <div v-else>
       <MyNavBar />
 
+<!-- '<iframe src="https://SUPERlongLINK" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>', -->
       <div class="stadiums-content d-flex flex-column align-items-center">
-        <p class="my-4 h2 text-danger font-weight-bold">Stadiums in this season!</p>
-        <ul>
-          <li v-for="(item, index) in stadiumsIn" :key="index">
-            {{ item }}
-          </li>
-        </ul>
-        <hr>
-        <ul>
-          <li v-for="(item, index) in stadiumsIn" :key="index">
-            {{ item }}
-          </li>
-        </ul>
+        <p class="my-4 h2 text-danger text-center font-weight-bold">Stadiums list</p>
+
+        <div class="container">
+          <div v-for="(item, index) in stadiumsNames" :key="index"
+            class="text-center">
+            <p class="m-0 mt-4 text-dark font-weight-bold">{{ clubNames[index] }}</p>
+
+            <!-- <b-btn @click="showCollapse = !showCollapse"
+                  :class="showCollapse ? 'collapsed' : null"
+                  aria-controls="collapse4"
+                  :aria-expanded="showCollapse ? 'true' : 'false'">
+              Toggle Collapse
+            </b-btn> -->
+
+            <!-- <b-btn @click="showCollapse = !showCollapse"
+                  :class="showCollapse ? 'collapsed' : null"
+                  aria-controls="collapse4"
+                  :aria-expanded="showCollapse ? 'true' : 'false'">
+              Toggle Collapse
+            </b-btn>
+            <b-collapse class="mt-2" v-model="showCollapse" id="collapse4">
+              <b-card>
+                I should start open!
+              </b-card>
+            </b-collapse> -->
+
+            <!-- <b-btn v-b-toggle.collapse1 variant="primary">Toggle Collapse</b-btn> -->
+            <b-btn v-b-toggle.collapse1 variant="danger" class="my-2">{{ item }}</b-btn>
+            <b-collapse id="collapse1" class="mt-2">
+              <b-card>
+                <iframe
+                  :src="stadiumsFrames[index]" width="250" height="250" frameborder="0"
+                  style="border:0" allowfullscreen class="card-text"></iframe>
+              </b-card>
+            </b-collapse>
+
+          </div>
+        </div>
+
       </div>
 
       <Arrows />
@@ -40,8 +68,10 @@ export default {
   },
   data() {
     return {
+      clubNames: null,
       stadiumsNames: null,
       stadiumsFrames: null,
+      showCollapse: true,
     };
   },
   computed: {
@@ -53,8 +83,10 @@ export default {
   methods: {
     getDataFromStore() {
       if (!this.$store.getters.isLoading) {
+        this.clubNames = this.$store.state.data.participating_clubs;
         this.stadiumsNames = this.$store.state.data.stadiums;
         this.stadiumsFrames = this.$store.state.stadiums;
+        // console.log(this.stadiumsFrames);
       }
     },
   },
@@ -74,5 +106,8 @@ export default {
     ul{
       list-style: none;
     }
+  }
+  .container{
+    width: 90vw;
   }
 </style>
